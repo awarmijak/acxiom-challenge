42234eb17bc5d479615f8e61842843a82390b42d
Warmijak <Aguszka9*>
2021-04-26 22:47:59 +0200

    Initial commit

---
+++ src/main/java/com/example/acxiomchallenge/AcxiomChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.acxiomchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcxiomChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcxiomChallengeApplication.class, args);
+    }
+
+}
